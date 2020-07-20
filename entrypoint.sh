f1f8d32999a123ad1621dc6624b6c95679bc9eda
Stav Shamir <shamir.stav@gmail.com>
2020-07-20 16:13:23 +0300

    Create index.html inside docs dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-resume export index.html --theme $1
-puppeteer --no-sandbox --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 print index.html resume.pdf
+mkdir -p /docs
+resume export docs/index.html --theme $1
+puppeteer --no-sandbox --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 print docs/index.html resume.pdf
